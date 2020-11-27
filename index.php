689e46b82a3000ebe6850ac9861b19bdc0d4ab62
reemss22 <75126174+reemss22@users.noreply.github.com>
2020-11-27 06:10:19 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>first page</title>
+</head>
+<body bgcolor="ff4433">
+<?php
+
+
+
+echo "بسم الله الله الرحمن الرحيم"
+echo "كليه الامارات للعلوم والتكنولوجيا"
+   echo "بكالاريوس تقانه المعلومات"
+   echo "حافظ سليمان"
+   echo "السكن: سوبا الحله"
+   echo "العمر: 25"
+    ?>
+</body>
+</html>
